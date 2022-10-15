a6ef5a5cda95ead772975bb7c7da127c2a926439
Desturo <lolomat25@gmai.com>
2022-10-15 03:54:05 +0200

    send message

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-//import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 
 const socket = io.connect("https://desturo.de");
@@ -7,15 +7,17 @@ const socket = io.connect("https://desturo.de");
 function App() {
 
   const sendMessage = () => {
-    console.log(socket.id)
+    socket.emit('message-send', message)
   }
+
+  const [message, setMessage] = useState('')
 
   
   
   return (
     <div>
       Test
-      <input placeholder='Message....'/>
+      <input placeholder='Message....' onChange={(e) => { setMessage(e.target.value) }}/>
       <button onClick={sendMessage}>Send Message</button>
       <div>Hallo das ist ein test</div>
     </div>
