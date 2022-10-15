99608d3f565f2c12e847744840938639dcf832d8
Desturo <lolomat25@gmai.com>
2022-10-15 05:36:07 +0200

    added dice and rolling dice

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,64 @@ function App() {
 
   const [recievedMessages, setRecievedMessages] = useState([])
   const [name, setName] = useState('')
+  const [dice, setDice] = useState([])
+  const [diceValue, setDiceValue] = useState(0)
   
   useEffect(() => {
     socket.on('message-recieve', (data) => {
-      setRecievedMessages([...recievedMessages, { user: data.user, message: data.message}])
+      setRecievedMessages([...recievedMessages, { user: data.user, message: data.message, dice: data.dice}])
     })
   }, [recievedMessages])
   
   
   const sendMessage = () => {
-    socket.emit('message-send', {user: name, message: message})
-    setRecievedMessages([...recievedMessages, {user: 'You', message: message}])
+    socket.emit('message-send', {user: name, message: message, dice: false})
+    setRecievedMessages([...recievedMessages, {user: 'Du', message: message, dice: false}])
     setMessage('')
   }
 
-  
+  const sendRoll = (diceNumber) => { 
+    var rolledNumber = Math.floor(Math.random() * diceNumber) + 1;
+    socket.emit('message-send', {user: name, message: rolledNumber, dice: true})
+    setRecievedMessages([...recievedMessages, { user: 'Du', message: rolledNumber, dice: true}])
+   }
+
+  const addDice = () => {
+    setDice([...dice, diceValue])
+  }
 
   const [message, setMessage] = useState('')
-
-  
   
   return (
     <div>
       <div>
         <input placeholder='Name....' onChange={(e) => { setName(e.target.value) }} value={name}/>
       </div>
-      Send a message:
+      Schicke eine Nachricht:
       <input placeholder='Message....' onChange={(e) => { setMessage(e.target.value) }} value={message}/>
-      <button onClick={sendMessage}>Send Message</button>
+      <button onClick={sendMessage}>Nachricht Abschicken</button>
+      <div>
+        <button onClick={addDice}>Würfel hinzufügen</button>
+        <input type="number" value={diceValue} onChange={(e) => { setDiceValue(e.target.value) }}/>
+        <div>{dice.map((item, key) => { return(<button key={"würfel"+key} onClick={(e) => { sendRoll(item) }} >W{item}</button>) })}</div>
+      </div>
       <div>
         <ul>
-          {recievedMessages.map((item, key) => { return(<li>{item.user + ' said: ' + item.message}</li>) })}
+          {recievedMessages.map((item, key) => { 
+            if(!dice) {
+              if(item.user === 'Du') {
+                return(<li key={"message"+key}>{'Du sagst: ' + item.message}</li>)
+              } else {
+                return(<li key={"message"+key}>{item.user + ' sagt: ' + item.message}</li>)
+              }
+            }else {
+              if(item.user === 'Du') {
+                return(<li key={"message"+key}>{'Du hast eine: ' + item.message + ' gewürfelt.'}</li>)
+              }else {
+                return(<li key={"message"+key}>{item.user + ' hat eine: ' + item.message + ' gewürfelt.'}</li>)
+              }
+            }
+          })}
         </ul>
       </div>
     </div>
